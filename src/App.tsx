bfdec011dd1aa6e7e35136d044954b3208360254
Chris Olsen <olsen.chris@gmail.com>
2024-07-04 16:51:32 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { GoAInput } from '@abgov/react-components'
+
+function App() {
+
+  return (
+    <>
+      <GoAInput name="foo" value="bar" onChange={() => {}} />
+    </>
+  )
+}
+
+export default App
